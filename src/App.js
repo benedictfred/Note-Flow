0e89366b434afa2e08ca3d4eb332865988525774
Ugochukwu Benedict Nebolisa <nebolisaugochukwu@gmail.com>
2024-07-31 04:52:43 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,123 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [openNote, setOpenNote] = useState(false);
+  const [notes, setNotes] = useState([]);
+  const [noteId, setNoteId] = useState(null);
+
+  function handleModal() {
+    setOpenNote(!openNote);
+  }
+
+  function handleAddNote(note) {
+    setNotes((notes) => [...notes, note]);
+  }
+
+  function handleSelectedNote(note) {
+    setNoteId(note);
+  }
+
+  function handleDeleteNote(id) {
+    setNotes((notes) => notes.filter((note) => note.id !== id));
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {openNote && <Modal onModal={handleModal} notes={noteId} />}
+      <Header />
+      <NoteList
+        onModal={handleModal}
+        notes={notes}
+        onSelectedNote={handleSelectedNote}
+        onDeleteNote={handleDeleteNote}
+      />
+      <Form onAddNote={handleAddNote} />
     </div>
+  );
+}
+
+function Header() {
+  return (
+    <header>
+      {" "}
+      <h1>NOTE FLOW</h1>
+    </header>
+  );
+}
+
+function NoteList({ notes, onModal, onSelectedNote, onDeleteNote }) {
+  if (notes.length === 0) return <h1 className="prefix">ADD NOTE</h1>;
+  return (
+    <ul className="notes">
+      {notes.map((notes) => (
+        <Note
+          onModal={onModal}
+          notes={notes}
+          key={notes.id}
+          onSelectedNote={onSelectedNote}
+          onDeleteNote={onDeleteNote}
+        />
+      ))}
+    </ul>
+  );
+}
+
+function Note({ notes, onModal, onSelectedNote, onDeleteNote }) {
+  function handleClicks(e) {
+    if (e.target.closest("button")) return;
+    onModal();
+    onSelectedNote(notes);
+  }
+  return (
+    <>
+      <li onClick={handleClicks}>
+        <p>{notes.note}</p>
+        <button onClick={() => onDeleteNote(notes.id)}>❌</button>
+      </li>
+    </>
+  );
+}
+
+function Modal({ notes, onModal }) {
+  return (
+    <>
+      <div className="overlay" onClick={onModal}></div>
+      <div className="modal">
+        <p>{notes?.note}</p>
+        <p className="cancel" onClick={onModal}>
+          {" "}
+          &times;{" "}
+        </p>
+      </div>
+    </>
+  );
+}
+
+function Form({ onAddNote }) {
+  const [note, setNote] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const newNote = {
+      note,
+      id: Date.now(),
+    };
+
+    onAddNote(newNote);
+
+    setNote("");
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <textarea
+        value={note}
+        placeholder="Add your note here"
+        rows="10"
+        cols="30"
+        onChange={(e) => setNote(e.target.value)}
+      />
+      <button>Add Note</button>
+    </form>
   );
 }
 
